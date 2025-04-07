6c8a8221f62a6982fbf7f578bbe7c0ae9d20e310
khushi <khushijain.bscibm2024@iar.ac.in>
2025-04-07 16:01:09 +0530

    file

---
+++ khushii.py
@@ -0,0 +1,36 @@
+<! DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Codepen - login &amp; singup form</title>
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="wrapper">
+  <div class="title-text">
+    <div class="title login">Login Form</div>
+    <div class="title signup">signup Form</div>
+  </div>
+      <div class="form-inner">
+      <form action="#" class="login">
+        <div class="field">
+          <input type="text" placeholder="Email Address" required>
+        </div>
+        <div class="field">
+          <input type="password" placeholder="password" required>
+        </div>
+        <div class="pass-link"><a href="#">Forgot password?</a></div>
+        <div class="field btn">
+          <div class="btn-layer"></div>
+          <input type="submit" value="Login">
+        </div>
+
+      </form>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
